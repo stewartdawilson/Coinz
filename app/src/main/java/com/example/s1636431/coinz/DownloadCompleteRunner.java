764d5814557a07bc5f8906e091f01431305aad1a
Stewart Wilson <stewart.wilson98@gmail.com>
2018-10-22 13:07:05 +0100

    Downloading file not working

--- app/src/main/java/com/example/s1636431/coinz/DownloadCompleteRunner.java
+++
@@ -1,9 +0,0 @@
-package com.example.s1636431.coinz;
-
-public class DownloadCompleteRunner {
-    static String result;
-    public static void downloadComplete(String result)
-    {
-        DownloadCompleteRunner.result = result;
-    }
-}
--- app/src/main/java/com/example/s1636431/coinz/MainActivity.java
+++ app/src/main/java/com/example/s1636431/coinz/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.s1636431.coinz;
 
+import android.annotation.SuppressLint;
 import android.graphics.Camera;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.location.LocationEngineListener;
@@ -22,12 +25,20 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
-import java.util.logging.Logger;
-import java.security.acl.Permission;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationEngineListener, PermissionsListener {
 
+    private String tag = "MainActivity";
     private MapView mapView;
     private MapboxMap map;
     private PermissionsManager permissionsManager;
@@ -36,15 +47,80 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private Location location;
     private Location originLocation;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       /* DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDateTime now = LocalDateTime.now();
+        String date = dtf.format(now);*/
+        String mapURL = "http://homepages.inf.ed.ac.uk/stg/coinz/2019/02/02/coinzmap.geojson";
+        Log.d("TEST", mapURL);
+        DownloadFileTask downloadTask = new DownloadFileTask();
+        downloadTask.execute(mapURL);
+
+
+
         Mapbox.getInstance(this, getString(R.string.access_token));
         setContentView(R.layout.activity_main);
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+    }
+
+    void downloadComplete(String result) {
+            result = result;
+    }
+
+
+    @SuppressLint("StaticFieldLeak")
+    class DownloadFileTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return loadFileFromNetwork(urls[0]);
+            } catch (IOException e) {
+                return
+                        "Unable to load content. Check your network connection"
+                        ;
+            }
+        }
+
+        private String loadFileFromNetwork(String urlString) throws IOException {
+            return readStream(downloadUrl(new URL(urlString)));
+        }
+
+        private InputStream downloadUrl(URL url) throws IOException {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000); // milliseconds
+            conn.setConnectTimeout(15000); // milliseconds
+            conn.setRequestMethod(
+                    "GET"
+            );
+            conn.setDoInput(true);
+            conn.connect();
+            return conn.getInputStream();
+        }
+
+        @NonNull
+        private String readStream(InputStream stream)
+                throws IOException {
+            // Read input from stream, build result as a string
+            StringBuilder sb = new StringBuilder();
+            BufferedReader r = new BufferedReader(new InputStreamReader(stream),1000);
+            for (String line = r.readLine(); line != null; line =r.readLine()){
+                sb.append(line);
+            }
+            stream.close();
+            return sb.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String result)
+        {
+            super.onPostExecute(result);
+            downloadComplete(result);
+        }
+
     }
 
     @Override
